a735383c4f432980e91845a6ea3b29bf2725e08f
Nick Noble <nnoble@me.com>
2024-12-11 13:06:53 -0600

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Nick Noble on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
